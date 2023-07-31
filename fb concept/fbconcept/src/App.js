7d9ef92c5a13e29c71cc6820d0e9f0e9ba727e6d
Aman Vishwakarma <115336951+vaman1846@users.noreply.github.com>
2023-07-31 15:33:21 +0530

    Update App.js

--- fb concept/fbconcept/src/App.js
+++ fb concept/fbconcept/src/App.js
@@ -10,60 +10,6 @@
 // const App = () => {
   
 
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path='/' element={<Login/>}></Route>
-//           <Route path='/Signup' element={<Signup/>}></Route>
-//           <Route path='/UploadImage' element={<UploadImage/>}></Route>
-//           <Route path='/UploadImage' element={<UploadImage  />}></Route>
-//          <Route path='/ImageGallery' element={<ImageGallery />} />
-//          <Route path='/Image' element={<ImageUploadForm/>}></Route>
-
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// // import React, { useState } from "react";
-// // import Signup from "./components/Signup/Signup";
-// // import Login from "./components/Login/Login";
-// // import UploadImage from "./components/UploadImage";
-// // import ImageGallery from "./components/ImageGallery";
-// // import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// // const App = () => {
-// //   const [imageUploaded, setImageUploaded] = useState(false);
-
-// //   const handleImageUpload = () => {
-// //     setImageUploaded(true);
-// //   };
-
-// //   return (
-// //     <div>
-// //       <BrowserRouter>
-// //         <Routes>
-// //           <Route path="/" element={<Login />}></Route>
-// //           <Route path="/Signup" element={<Signup />}></Route>
-// //           <Route
-// //             path="/UploadImage"
-// //             element={<UploadImage onImageUpload={handleImageUpload} />}
-// //           ></Route>
-
-// //           <Route path="/ImageGallery" element={<ImageGallery />} />
-// //         </Routes>
-// //       </BrowserRouter>
-// //     </div>
-// //   );
-// // };
-
-// // export default App;
-
-
 
 
 import React, { useState } from "react";
